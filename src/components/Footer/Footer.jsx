d553271506006c2cdf7d514875107e440378a087
Katt <kitcat0962@gmail.com>
2025-04-14 07:51:25 -0400

    removed footer-spacer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,7 @@
 import './Footer.scss'
 
 export default function Footer({ }) {
-    return <>
-        <div className='footer-spacer'></div>
-        <footer className="footer">
-            <span className="title">Footer</span>
-        </footer>
-    </>
+    return <footer className="footer">
+        <span className="title">Footer</span>
+    </footer>
 }
