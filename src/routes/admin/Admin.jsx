62717c69781d6fd9b860877df8067ec1213d23ac
Katt <kitcat0962@gmail.com>
2025-04-15 12:26:52 -0400

    implimented actual admin authentication * included a copy of the firebase rules so I can reference in the future

--- src/routes/admin/Admin.jsx
+++ src/routes/admin/Admin.jsx
@@ -2,36 +2,61 @@ import styles from './Admin.module.scss'
 import { Outlet, useNavigate } from 'react-router'
 import Login from './Login'
 import { useEffect, useState } from 'react'
-import { auth } from '../../scripts/firebase'
+import { auth, db } from '../../scripts/firebase'
 import { signOut } from 'firebase/auth'
 import Button from '../../components/Buttons/Button'
 import Spinner from '../../components/Spinner/Spinner'
 import Spacer from '../../components/Spacer'
 import Center from '../../components/Center'
+import { doc, getDoc, onSnapshot } from 'firebase/firestore'
 export default function Admin({ }) {
     const navigate = useNavigate()
+    const [attemptedAuth, setAttemptedAuth] = useState(false)
     const [user, setUser] = useState(null)
-    const [authAttempted, setAuthAttempted] = useState(false)
+    const [admin, setAdmin] = useState(null)
     useEffect(() => {
-        auth.onAuthStateChanged(user => {
-            setAuthAttempted(true)
+        return auth.onAuthStateChanged(user => {
+            setAttemptedAuth(true)
             setUser(user)
-            if (!user) {
+            if (!user)
                 navigate('/admin')
-            }
         })
     }, [])
-    return authAttempted ?
-        user ?
-            <div className={styles.admin}>
-                <div className={styles.buttons}>
-                    <Button onClick={() => navigate('orders')}>Orders?</Button>
-                    <Button onClick={() => navigate('products')}>Products?</Button>
-                    <Spacer />
-                    <Button onClick={() => signOut(auth)} className={styles.logout}>Logout</Button>
-                </div>
-                <Outlet />
-            </div> :
-            <Login /> :
-        <Center><Spinner /></Center>
+    useEffect(() => {
+        if (!user) {
+            setAdmin(null)
+            return
+        }
+        return onSnapshot(doc(db, 'users', user.uid), doc => setAdmin(doc.get('admin') ?? false))
+    }, [user])
+
+    // Page load, waiting for firebase auth to start
+    if (!attemptedAuth)
+        return <Center><Spinner /></Center>
+    // Nobody logged in currently. Show Login
+    if (!user)
+        return <Login />
+    // Somebody is logged in. Waiting to fetch admin data
+    if (admin == null)
+        return <Center><Spinner /></Center>
+    // Admin data fetched. This user is not an admin
+    if (!admin)
+        return <Center>
+            <Spacer size={4} />
+            <h2>Unauthorized</h2>
+            <Spacer />
+            <Button onClick={() => signOut(auth)} className={styles.logout}>Logout</Button>
+            <Spacer size={4} />
+        </Center>
+
+    return <div className={styles.admin}>
+        <div className={styles.buttons}>
+            <Button onClick={() => navigate('orders')}>Orders?</Button>
+            <Button onClick={() => navigate('products')}>Products?</Button>
+            <Spacer />
+            <Button onClick={() => signOut(auth)} className={styles.logout}>Logout</Button>
+        </div>
+        <Outlet />
+    </div>
+
 }
