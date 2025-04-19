68b66c3994643714a94fdeb8580964ef18ecb67e
Katt <kitcat0962@gmail.com>
2025-04-19 06:05:48 -0400

    cart counts total items instead of unique items

--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -3,13 +3,14 @@ import styles from './CartButton.module.scss'
 import { useNavigate } from 'react-router'
 
 export default function CartButton({ useCart }) {
-    const [cart, setProduct] = useCart
+    const [cart] = useCart
     const navigate = useNavigate()
+    const cartCount = () => cart.reduce((sum, { count }) => sum + count, 0)
     return <div className={styles.cart} onClick={() => navigate('/cart')}>
         <div className={styles.iconbox}>
             <div className={styles.icon}><FaShoppingCart /></div>
             {cart.length > 0 && <span className={styles.number}>
-                {cart.length}
+                {cartCount()}
             </span>}
         </div>
         <span className={styles.label}>Cart</span>
